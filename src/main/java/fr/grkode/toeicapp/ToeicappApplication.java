4f1a53584cd73dffa543567f60cdb97ced58c61a
Roméo GBONGBON <romeo.gbongbon@gmail.com>
2025-06-13 16:06:17 +0200

    project init

---
+++ src/main/java/fr/grkode/toeicapp/ToeicappApplication.java
@@ -0,0 +1,13 @@
+package fr.grkode.toeicapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToeicappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToeicappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/grkode/toeicapp/ToeicappApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.grkode.toeicapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToeicappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
